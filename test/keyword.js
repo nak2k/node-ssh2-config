d7ae4b221548c6a3886890d68824c41e7f66ad9f
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2017-08-01 00:11:50 +0900

    Fix arguments parsing for LocalForward and RemoteForward #84

---
+++ test/keyword.js
@@ -0,0 +1,28 @@
+const test = require('tape');
+const { parseConfigLine } = require('../lib/parser');
+
+test('test LocalForward', t => {
+  t.plan(3);
+
+  const line = 'LocalForward  80 localhost:80';
+  const context = { result: {} };
+
+  parseConfigLine(line, context);
+
+  t.error(context.error);
+  t.notEqual(context.result.LocalForward, undefined);
+  t.equal(context.result.LocalForward.length, 3);
+});
+
+test('test RemoteForward', t => {
+  t.plan(3);
+
+  const line = 'RemoteForward  80 localhost:80';
+  const context = { result: {} };
+
+  parseConfigLine(line, context);
+
+  t.error(context.error);
+  t.notEqual(context.result.RemoteForward, undefined);
+  t.equal(context.result.RemoteForward.length, 3);
+});
